f2d786773313cbdfff7d2401dc52cde4820186cd
Mahdi <mahdirefaideen@gmail.com>
2019-04-21 22:24:23 +0530

    create account page

---
+++ app/src/main/java/android/g6/cricspot/CreateAccountActivity.java
@@ -0,0 +1,48 @@
+package android.g6.cricspot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    TextView nameTxt, userNameTxt, passwordTxt, ageTxt, phoneTxt, errTxt;
+    EditText nameE, userNameE, passwordE, ageE, phoneE;
+    String name, userName, password, age, phone;
+    Button createAccountBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        /* Initialize variables */
+        nameTxt = findViewById(R.id.nameTxtInCreateAccountPage);
+        userNameTxt = findViewById(R.id.userNameTxtInCreateAccountPage);
+        passwordTxt = findViewById(R.id.passwordTxtInCreateAccountPage);
+        ageTxt = findViewById(R.id.ageTxtInCreateAccountPage);
+        phoneTxt = findViewById(R.id.phoneTxtInCreateAccountPage);
+        errTxt = findViewById(R.id.errorTxtInCreateAccountPage);
+
+        nameE = findViewById(R.id.nameInCreateAccountPage);
+        userNameE = findViewById(R.id.userNameInCreateAccountPage);
+        passwordE = findViewById(R.id.passwordInCreateAccountPage);
+        ageE = findViewById(R.id.ageInCreateAccountPage);
+        phoneE = findViewById(R.id.phoneInCreateAccountPage);
+
+        createAccountBtn = findViewById(R.id.createAccountBtnInCreateAccountPage);
+    }
+
+    public void createAccountClickedInCreateAccountPage(View view) {
+
+        /* Get values from user */
+        name = nameE.getText().toString();
+        userName = userNameE.getText().toString();
+        password = passwordE.getText().toString();
+        age = ageE.getText().toString();
+        phone = phoneE.getText().toString();
+    }
+}
